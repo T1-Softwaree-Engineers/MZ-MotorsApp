584de5833cfd445b0ac5dbd4a9b8b1fd136ddd29
dorian11-byte <draygoza@icloud.com>
2022-10-14 19:37:44 -0500

    home screen item working by click and navigate to the car details screen

--- app/src/main/java/com/example/mz_motorsport/home.java
+++ app/src/main/java/com/example/mz_motorsport/home.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
@@ -13,18 +13,23 @@ import androidx.fragment.app.Fragment;
 public class home extends Fragment {
 
     View vista;
-    RelativeLayout post;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         vista = inflater.inflate(R.layout.fragment_home, container, false);
+
+        ImageView imgcar = (ImageView) vista.findViewById(R.id.imgcar);
+        imgcar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(),CarDetails.class);
+                startActivity(i);
+            }
+        });
+
         return vista;
 
     }
 
-    public void clickPost(View view) {
-        Intent i = new Intent(this, navigation.class);
-
-    }
 }
