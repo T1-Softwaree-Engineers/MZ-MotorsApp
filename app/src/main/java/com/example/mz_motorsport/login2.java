77ab41085379eaee4aba87b5975d8eb1ff88a6f7
Jk99z2 <eochoa11@ucol.mx>
2022-10-20 23:35:13 -0500

    Login y registro funcional con MySQL, PHP API y Android Volley

--- app/src/main/java/com/example/mz_motorsport/login2.java
+++ app/src/main/java/com/example/mz_motorsport/login2.java
@@ -6,25 +6,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class login2 extends AppCompatActivity {
 
     TextView signup;
-    TextInputEditText et_username,et_password;
+    TextInputEditText et_username, et_password;
     Button btn_login;
 
-    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +46,6 @@ public class login2 extends AppCompatActivity {
         et_password = (TextInputEditText)findViewById(R.id.et_password);
         btn_login = (Button)findViewById(R.id.btnlogin);
 
-        mAuth = FirebaseAuth.getInstance();
 
         //Ir al SignUp
         signup.setOnClickListener(new View.OnClickListener() {
@@ -51,37 +60,60 @@ public class login2 extends AppCompatActivity {
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                loginUser();
+                loginUser("https://ochoarealestateservices.com/mzmotors_api/db/validation.php");
             }
         });
 
 
     }
 
-    private void loginUser(){
-        String email, password;
-        email = et_username.getText().toString();
-        password = et_password.getText().toString();
+    private void loginUser(String URL)
+    {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
 
-        if (TextUtils.isEmpty(email)) {
-            et_username.setError("Ingrese un Email");
-            et_username.requestFocus();
-        }else if (TextUtils.isEmpty(password)) {
-            et_password.setError("Ingrese su contraseña");
-            et_password.requestFocus();
-        }else {
-            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
-                        Toast.makeText(login2.this, "Inicio de Sesion Exitoso", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(login2.this, navigation.class));
-                        finish();
-                    }else {
-                        Toast.makeText(login2.this, "Error de Inicio de Sesion: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                    }
+                if(response.equals("no_email"))
+                {
+                    Toast.makeText(login2.this, "Ingrese un email", Toast.LENGTH_SHORT).show();
                 }
-            });
-        }
+                if(response.equals("empty_password"))
+                {
+                    Toast.makeText(login2.this, "Ingrese una contraseña", Toast.LENGTH_SHORT).show();
+                }
+                else if(response.equals("empty_data"))
+                {
+                    Toast.makeText(login2.this, "Rellene todos los campos", Toast.LENGTH_SHORT).show();
+                }
+
+                else if(response.equals("1"))
+                {
+                    Intent intent = new Intent(getApplicationContext(), navigation.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else if(response.equals("no_verif"))
+                {
+                    Toast.makeText(login2.this, "Email o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(login2.this, error.toString(), Toast.LENGTH_LONG).show();
+                String text = error.toString();
+                Log.e("error",text);
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("email", et_username.getText().toString());
+                parametros.put("password", et_password.getText().toString());
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
--- app/src/main/java/com/example/mz_motorsport/signup2.java
+++ app/src/main/java/com/example/mz_motorsport/signup2.java
@@ -6,24 +6,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class signup2 extends AppCompatActivity {
 
     TextView login;
     EditText et_name, et_email, et_phone, et_password;
     Button btn_Signup;
-
-    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +46,11 @@ public class signup2 extends AppCompatActivity {
         et_password = (EditText)findViewById(R.id.et_password);
         btn_Signup = (Button)findViewById(R.id.btnsignup);
 
-        mAuth = FirebaseAuth.getInstance();
-
         //Boton Registrarse
         btn_Signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                createUser();
+                registrarUser("https://ochoarealestateservices.com/mzmotors_api/db/register.php");
             }
         });
 
@@ -57,34 +64,51 @@ public class signup2 extends AppCompatActivity {
         });
     }
 
-    private void createUser(){
-        String name,email,phone,password;
-        name = et_name.getText().toString();
-        email = et_email.getText().toString();
-        phone = et_phone.getText().toString();
-        password = et_password.getText().toString();
+    private void registrarUser(String URL){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
 
-        if (TextUtils.isEmpty(email)) {
-            et_name.setError("El Email no puede estar vacio");
-            et_name.requestFocus();
-        }else if (TextUtils.isEmpty(password)) {
-            et_name.setError("La Contraseña no puede estar vacia");
-            et_name.requestFocus();
-        }else {
-            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
-                        Toast.makeText(signup2.this, "Usuario Registrado correctamente", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(signup2.this, login2.class));
-                        finish();
-                    }else {
-                        Toast.makeText(signup2.this, "Error de Registro: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                    }
+                if(response.equals("empty_name"))
+                {
+                    Toast.makeText(signup2.this, "Ingrese un nombre", Toast.LENGTH_SHORT).show();
                 }
-            });
-        }
+                if(response.equals("empty_password") || response.equals("pass_cannot_be_zero"))
+                {
+                    Toast.makeText(signup2.this, "Ingrese una contraseña", Toast.LENGTH_SHORT).show();
+                }
+                else if(response.equals("emtpy_contacto"))
+                {
+                    Toast.makeText(signup2.this, "Rellene todos los campos", Toast.LENGTH_SHORT).show();
+                }
 
+                else if(response.equals("register_success"))
+                {
+                    Toast.makeText(signup2.this, "Registro valido", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), login2.class);
+                    startActivity(intent);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(signup2.this, error.toString(), Toast.LENGTH_LONG).show();
+                String text = error.toString();
+                Log.e("error",text);
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("nombre", et_name.getText().toString());
+                parametros.put("email", et_email.getText().toString());
+                parametros.put("contacto", et_phone.getText().toString());
+                parametros.put("password", et_password.getText().toString());
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
     @Override
